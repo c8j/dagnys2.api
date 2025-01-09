8936101d95dc52659fe9daee1cc02fe1c56f7549
CalinJilavu <23583666+c8j@users.noreply.github.com>
2025-01-09 11:39:49 +0100

    Create controllers

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dagnys2.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
