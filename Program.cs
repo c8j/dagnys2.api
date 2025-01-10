411c12e5e38ca2e3903b18e4e9084b312d09c7ae
CalinJilavu <23583666+c8j@users.noreply.github.com>
2025-01-10 19:20:43 +0100

    Add swagger and https

--- Program.cs
+++ Program.cs
@@ -12,7 +12,18 @@ builder.Services.AddDbContext<DataContext>(
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
